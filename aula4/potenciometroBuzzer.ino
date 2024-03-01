8ca52fd3a852211bc377059ffb5b93bc7441f2a8
João Pedro Souza Arruda <joao.arruda.jope16@gmail.com>
2024-03-01 04:56:11 -0300

    new class

---
+++ aula4/potenciometroBuzzer.ino
@@ -0,0 +1,25 @@
+const int pinPot = A5; // Indica a porta analógica ligada ao potenciômetro
+const int pinBuzzer = 9; // Indica a porta digital ligada ao buzzer (PWM)
+
+void setup(){
+  pinMode(pinBuzzer, OUTPUT);
+  pinMode(pinPot, INPUT); // Por padrão porta analógica já é INPUT
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  int potValue = analogRead(pinPot); // Lê valores no potenciômetro
+  int pwm = map(potValue, 0, 1024, 0, 255); //define valores pwm (0 a 255)
+  float volt = potValue * (5.00/1023.0); //Valores da tensão no circuito
+  
+  Serial.print("V = ");
+  Serial.print(potValue);
+  Serial.print(" | T = ");
+  Serial.print(volt);
+  Serial.print(" | PWM = ");
+  Serial.println(pwm);
+
+  analogWrite(pinBuzzer, pwm);
+     
+}
